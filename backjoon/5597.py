b3f3e1c3e50228e868c6c73c2d8a7ac4eb672849
bbugi <chj1491@naver.com>
2023-01-11 12:54:55 +0900

    Update 5597.py

--- backjoon/5597.py
+++ backjoon/5597.py
@@ -1,6 +1,28 @@
-# 과제 안 내신 분..?
+# 과제 안 내신 분..? (브5)
 # https://www.acmicpc.net/problem/5597
 
 # ========= 문제 풀이 ==========
 
+# 1번부터 30번까지 출석번호 존재
+# 입력은 28명만 제출
+# 제출안한 2명의 출석번호를 작은 순서로 2줄로 출력하라
 
+import sys
+input = sys.stdin.readline
+
+a = list(range(1,31))
+# print(a)
+for _ in range(28):
+    n = int(input())
+    a.remove(n)
+    # print(n, a)
+print(*a, sep='\n')
+print(a, sep='\n')
+
+
+# =========== 참조 =============
+
+# # *리스트 : 어퍼스트로피 사용
+# a = [1,2,3,4,5]
+# print('print a:',a, sep='\n')
+# print('print(*a) : ', *a, sep='\n')
