7438327048db1b998661a1db13debcd462d6fb3b
bbugi <chj1491@naver.com>
2023-01-13 18:57:01 +0900

    update 239113 2

--- backjoon/10989(완).py
+++ backjoon/10989(완).py
@@ -5,27 +5,27 @@
 # 오름차순 정렬 결과 출력
 # 주어지는 수는 10,000보다 작거나 같은 자연수
 
-# ===== 문제 풀이 2 - 계수 정렬 코드 사용 - 메모리 초과 발생 =====
+# ===== 문제 풀이 2 - 계수 정렬 사용 - 맞춤 =====
 import sys
 
 n = int(sys.stdin.readline())
 count = [0] * (10000+1)
 
-array = []
+# 리스트에 담는 만큼 메모리를 잡아먹는다.
+# 굳이 리스트에 값을 담을 필요는 없다
+
+# array = []
+# for _ in range(n):
+#     array.append(int(sys.stdin.readline()))
+
 for _ in range(n):
-    array.append(int(sys.stdin.readline()))
-
+    num = int(sys.stdin.readline())
+    count[num] += 1
 # print(array)
-
-for i in range (len(array)):
-    count[array[i]] += 1
 
 for i in range(len(count)):
     for j in range(count[i]):
         print(i)
-
-
-
 
 # ==== 문제 풀이1 - 메모리 초과 발생 ====
 
