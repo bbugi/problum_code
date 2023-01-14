bca5db93b1fa1293c8f96a03613b7540266278b6
bbugi <chj1491@naver.com>
2023-01-15 06:27:49 +0900

    update 230115 commit 2

---
+++ backjoon/15596(완).py
@@ -0,0 +1,25 @@
+# 정수 N개의 합 (브2)
+# https://www.acmicpc.net/problem/15596
+
+'''
+프로그래밍 언어 제한 문제
+함수 작성 (def) 문제
+'''
+
+# ==== 문제 풀이 ====
+
+def solve(a):
+    return sum(a)
+
+
+
+# === 문제 이해 잘못해서 푼 방식 ===
+# def sum_n(n):
+#     a = []
+#     for i in range(n):
+#         a.append(int(input()))
+#     return sum(a)
+
+
+
+
