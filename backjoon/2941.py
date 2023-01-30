147284ce0a9fcc34c135e52d2ca2afb9d0d3ac75
bbugi <chj1491@naver.com>
2023-01-30 17:33:57 +0900

    Create 2941.py

---
+++ backjoon/2941.py
@@ -0,0 +1,15 @@
+# 크로아티아 알파벳
+# https://www.acmicpc.net/problem/2941
+
+'''
+구현, 문자열
+'''
+
+
+# ===== 문제 풀이 =====
+
+# 크로아티아 문자열
+alp_list = list('abcdefghijklmnopqrstuvwxyz-=')
+alp_list.extend(['c=','c-','dz=','d-','lj','nj','s=','z='])
+print(alp_list)
+
